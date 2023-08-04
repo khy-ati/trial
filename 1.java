298e2fe70897700971b09a5dbf44acf75cf809ff
Khyati Sharma <khyatis.0204@gmail.com>
2023-08-04 17:51:08 +0530

    Initial commit

---
+++ 1.java
@@ -0,0 +1,12 @@
+class message {
+    String msg = "Hello World";
+    
+    public static void main(String[] args) {
+        message obj = new message();
+        System.out.println(obj.msg);
+
+        
+    }
+
+    
+}
