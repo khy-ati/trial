6dda28f77e8f8c175f910a39c7f774df20c3c92e
Khyati Sharma <khyatis.0204@gmail.com>
2023-08-04 18:10:03 +0530

    Message modified

--- 1.java
+++ 1.java
@@ -1,11 +1,10 @@
+import java.util.Scanner;
+
 class message {
-    String msg = "Hello World";
-    
+    String msg = "Hello!!";
     public static void main(String[] args) {
         message obj = new message();
         System.out.println(obj.msg);
-
-        
     }
 
     
